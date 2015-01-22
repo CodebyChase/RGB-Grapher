918ae40004337d2a83c3a530c12b7c1174c3fa33
Chase Moran <chamora93@gmail.com>
2015-01-22 05:30:54 -0500

    initial commit

---
+++ src/Core.java
@@ -0,0 +1,16 @@
+/**
+* @author chase
+* Contains main and executes the rest of the program
+*/
+public class Core {
+
+	/**
+	 * @param args is not used
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
